f90c344212ce5ab35c8772d2a8b3e4eca3fcdaf9
Valentino Isgro <valentinoisgro@gmail.com>
2025-09-19 01:45:09 -0300

    Refactor dtoReview function for improved readability and consistency in variable declaration

--- src/utils/review.utils.js
+++ src/utils/review.utils.js
@@ -1,7 +1,15 @@
 export const dtoReview = review => {
   // Eliminar información sensible o innecesaria, pero incluir status
-  const { id, userId, productId, rating, comment, status, createdAt, updatedAt } =
-    review
+  const {
+    id,
+    userId,
+    productId,
+    rating,
+    comment,
+    status,
+    createdAt,
+    updatedAt,
+  } = review
   return {
     id,
     userId,
